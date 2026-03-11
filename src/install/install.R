97c00b56078cad54124c09997f4df9ce1bdf81d1
harrymatthews50 <harrymatthews50@gmail.com>
2026-03-11 15:29:13 +0100

    add generuc preprocessing pipleine and tests

--- src/install/install.R
+++ src/install/install.R
@@ -1,3 +1,6 @@
 BiocManager::install("limma")
+BiocManager::install("edgeR") # prerequisite for sva
+BiocManager::install("sva")
+BiocManager::install("preprocessCore")
 install.packages("readr")
 install.packages("dplyr")

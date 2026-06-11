3d20d0657a4db0eda6ede9a22c96b06378766efe
https://git.dkfz.de/h600s <harold.matthews@dkfz-heidelberg.de>
2026-06-11 16:32:34 +0200

    remove dependency on preprocessCore by using limma normalizeQuantiles instead

--- src/install/install.R
+++ src/install/install.R
@@ -1,7 +1,6 @@
 BiocManager::install("limma")
 BiocManager::install("edgeR") # prerequisite for sva
 BiocManager::install("sva")
-BiocManager::install("preprocessCore")
 install.packages("readr")
 install.packages("dplyr")
 install.packages("jsonlite")
